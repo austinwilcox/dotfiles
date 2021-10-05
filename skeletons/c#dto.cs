c035d31c227f360988122d805dad55b15ae25667
Austin <austin21wilcox@gmail.com>
2021-10-05 09:36:34 -0600

    Added in a c# class skeleton

---
+++ skeletons/c#dto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADDHERE
+{
+  public class DTO
+  {
+    [Required]
+    public string Field { get; set; }
+  }
+}
