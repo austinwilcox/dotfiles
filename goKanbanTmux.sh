777dc5d05e87f7d9f6690b28812fa7d113eb1181
Austin <awilcox@arbinger.com>
2022-02-21 23:02:59 -0700

    changed the tmux config to use air.

--- goKanbanTmux.sh
+++ goKanbanTmux.sh
@@ -3,5 +3,5 @@ tmux new-session -d -s GoKanban
 tmux rename-window server
 tmux send "cd ~/Software/go-kanban && go run ." C-m
 tmux neww -n 'Code'
-tmux send "cd ~/Software/go-kanban && nvim ." C-m
+tmux send "cd ~/Software/go-kanban && air" C-m
 tmux a
