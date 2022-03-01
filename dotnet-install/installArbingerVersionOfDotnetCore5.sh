51d5f85962d4dfafac19f1222b4e56c92e92fe7c
Austin <awilcox@arbinger.com>
2022-03-01 11:42:41 -0700

    added dotnet 6 to my install path

--- dotnet-install/installArbingerVersionOfDotnetCore5.sh
+++ dotnet-install/installArbingerVersionOfDotnetCore5.sh
@@ -1,2 +1,3 @@
-#!bin/bash
+#!/bin/bash
 sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 5.0.xxx -version 5.0.403
+sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 6.0.xxx -version 6.0.200
