ce90e7118e4031f7eab34010d5e5429a41e0b76d
Austin <awilcox@arbinger.com>
2022-07-05 08:39:17 -0600

    moving things around and deleting unnecessary files.

--- arbPortalUnitTestsTmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-tmux new-session -d -s UnitTests
-tmux rename-window runner
-tmux send "cd ~/Software/arbinger/server/UnitTests/" C-m
-tmux neww -n "project"
-tmux send "cd ~/Software/arbinger/server/UnitTests/ && nvim ." C-m
-tmux a
--- goKanbanTmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-tmux new-session -d -s GoKanban
-tmux rename-window server
-tmux send "cd ~/Software/go-kanban && air" C-m
-tmux neww -n 'Code'
-tmux send "cd ~/Software/go-kanban && nvim ." C-m
-tmux a
