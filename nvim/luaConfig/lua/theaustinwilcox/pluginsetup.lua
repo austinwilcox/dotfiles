6f089c7ec5edf2d026716b4de27c120ca2fcd992
Austin Wilcox <austin21wilcox@gmail.com>
2023-06-05 10:32:42 -0600

    removed nonicons. Added prettier nvim.

--- nvim/luaConfig/lua/theaustinwilcox/pluginsetup.lua
+++ nvim/luaConfig/lua/theaustinwilcox/pluginsetup.lua
@@ -3,9 +3,6 @@
 -- let g:vimwiki_list = [{'syntax': 'markdown', 'ext': '.md'}]
 -- ]]
 
--- nvim-noicons setup
-require('nvim-nonicons').setup {}
-
 -- Ultisnips setup
 vim.cmd[[
 let g:UltiSnipsExpandTrigger='<tab>'
@@ -136,6 +133,61 @@ require('goto-preview').setup {
 }
 
 require("mason").setup()
+
+--Setup null-ls
+local null_ls = require("null-ls")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+})
+
+--Setup prettier
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
 
 -- local dap = require'dap'
 -- dap.adapters.coreclr = {
