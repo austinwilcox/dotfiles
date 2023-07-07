010c0245e0f419107dab167033660c3651c3960d
Austin Wilcox <austin21wilcox@gmail.com>
2023-07-07 14:50:23 -0600

    got copilot working.

--- nvim/luaConfig/lua/theaustinwilcox/set.lua
+++ nvim/luaConfig/lua/theaustinwilcox/set.lua
@@ -50,6 +50,9 @@ vim.opt.modifiable = true
 vim.opt.background = "dark"
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
+--Copilot setup
+vim.g.copilot_assume_mapped = true
+
 -- Options that I don't know how to toggle with lua
 vim.cmd [[
   let g:rainbow_active=1
