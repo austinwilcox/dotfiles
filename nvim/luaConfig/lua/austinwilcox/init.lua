6e31de33e34b6c75e690151086f0cc849b719dd5
Austin Wilcox <austin21wilcox@gmail.com>
2023-10-04 14:23:59 -0600

    added typescript react snippets, fixed up colorscheme

--- nvim/luaConfig/lua/austinwilcox/init.lua
+++ nvim/luaConfig/lua/austinwilcox/init.lua
@@ -1,6 +1,5 @@
 require('austinwilcox.set')
 require("austinwilcox.keymaps")
-require("austinwilcox.colorscheme")
 require("austinwilcox.plugins")
 require("austinwilcox.pluginsetup")
 require("austinwilcox.luasnip-config")
--- nvim/luaConfig/lua/austinwilcox/plugins.lua
+++ nvim/luaConfig/lua/austinwilcox/plugins.lua
@@ -104,7 +104,14 @@ local plugins = {
     lazy=false,
     priority=1000,
     config = function()
-      vim.cmd([[colorscheme tokyonight]])
+      -- vim.cmd([[colorscheme tokyonight]])
+      local colorscheme = "tokyonight-storm"
+
+      local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+      if not status_ok then
+        vim.notify("colorscheme " .. colorscheme .. " not found!")
+        return
+      end
     end
   },
   {
