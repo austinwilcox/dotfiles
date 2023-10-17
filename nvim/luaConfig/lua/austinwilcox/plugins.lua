10e1ac66e0bdb63c14c04d895c51371020cbe24b
Austin Wilcox <austin21wilcox@gmail.com>
2023-10-17 08:55:33 -0600

    added plugin.

--- nvim/luaConfig/lua/austinwilcox/plugins.lua
+++ nvim/luaConfig/lua/austinwilcox/plugins.lua
@@ -569,34 +569,14 @@ local plugins = {
       }
     end
   },
-{
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
+  {
+    "nvim-neotest/neotest",
     dependencies = {
-        -- "nvim-treesitter/nvim-treesitter",
-        -- "nvim-telescope/telescope.nvim",
-        -- "nvim-lua/plenary.nvim", -- required by telescope
-        -- "MunifTanjim/nui.nvim",
-
-        -- -- optional
-        -- "nvim-tree/nvim-web-devicons",
-
-        -- -- recommended
-        -- "rcarriga/nvim-notify",
-    },
-    opts = {
-        -- configuration goes here
-    },
-    config = function(_, opts)
-        vim.keymap.set("n", "<leader>lq", "<cmd>LcQuestionTabs<cr>")
-        vim.keymap.set("n", "<leader>lm", "<cmd>LcMenu<cr>")
-        vim.keymap.set("n", "<leader>lc", "<cmd>LcConsole<cr>")
-        vim.keymap.set("n", "<leader>ll", "<cmd>LcLanguage<cr>")
-        vim.keymap.set("n", "<leader>ld", "<cmd>LcDescriptionToggle<cr>")
-
-        require("leetcode").setup(opts)
-    end,
-}
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim"
+    }
+  }
 }
 
 local opts = {}
