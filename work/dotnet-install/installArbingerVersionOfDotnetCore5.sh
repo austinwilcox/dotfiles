76a1c36d32419aa5a3baa2ded1efbc69e8457d58
Austin Wilcox <me@theaustinwilcox.com>
2024-11-10 20:25:00 -0700

    update dotnet install script

--- work/dotnet-install/installArbingerVersionOfDotnetCore5.sh
+++ work/dotnet-install/installArbingerVersionOfDotnetCore5.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 5.0.xxx -version 5.0.403
-sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 6.0.xxx -version 6.0.302
+# sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 6.0.xxx -version 6.0.302
+sudo ./dotnet-install.sh --install-dir /usr/share/dotnet -channel 8.0.xxx -version 8.0.403
