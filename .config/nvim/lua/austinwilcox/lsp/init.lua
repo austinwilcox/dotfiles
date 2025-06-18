41f7799a8e45e7fbb23f733f18d72e1ccbabf3c9
Austin Wilcox <austin21wilcox@gmail.com>
2025-06-18 14:53:57 -0600

    trying to get lsp working again

--- .config/nvim/lua/austinwilcox/lsp/init.lua
+++ .config/nvim/lua/austinwilcox/lsp/init.lua
@@ -42,29 +42,28 @@ local custom_attach = function(client)
   -- }, buffer_number)
 
   --Older native lsp configuration options
-  buf_nnoremap { "K", vim.lsp.buf.hover }
-  buf_nnoremap { "gd", vim.lsp.buf.definition }
-  buf_nnoremap { "]d", vim.diagnostic.goto_next }
-  buf_nnoremap { "[d[", vim.diagnostic.goto_prev }
-  buf_nnoremap { "<leader>gr", vim.lsp.buf.rename }
-  buf_nnoremap { "<leader>gR", vim.lsp.buf.references }
-  buf_nnoremap { "gt", vim.lsp.buf.type_definition }
-  buf_nnoremap { "gi", vim.lsp.buf.implementation }
-  buf_nnoremap { "<leader>gl", "<cmd>Telescope diagnostics<cr>" }
-  buf_nnoremap { "<leader>ca", vim.lsp.buf.code_action }
+  buf_nnoremap({ "K", vim.lsp.buf.hover })
+  buf_nnoremap({ "gd", vim.lsp.buf.definition })
+  buf_nnoremap({ "]d", vim.diagnostic.goto_next })
+  buf_nnoremap({ "[d[", vim.diagnostic.goto_prev })
+  buf_nnoremap({ "<leader>gr", vim.lsp.buf.rename })
+  buf_nnoremap({ "<leader>gR", vim.lsp.buf.references })
+  buf_nnoremap({ "gt", vim.lsp.buf.type_definition })
+  buf_nnoremap({ "gi", vim.lsp.buf.implementation })
+  buf_nnoremap({ "<leader>gl", "<cmd>Telescope diagnostics<cr>" })
+  buf_nnoremap({ "<leader>ca", vim.lsp.buf.code_action })
 end
 
-require "lsp_signature".setup({
+require("lsp_signature").setup({
   bind = true, -- This is mandatory, otherwise border config won't get registered.
   handler_opts = {
-    border = "rounded"
-  }
+    border = "rounded",
+  },
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-
 
 -- Rust Setup
 -- require'lspconfig'.rust_analyzer.setup({
@@ -73,20 +72,20 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- })
 
 -- Lua Setup
-require 'lspconfig'.lua_ls.setup({
+require("lspconfig").lua_ls.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
   filetypes = { "lua" },
   settings = {
     Lua = {
       diagnostics = {
-        globals = { "vim" }
-      }
-    }
-  }
+        globals = { "vim" },
+      },
+    },
+  },
 })
 
-require 'lspconfig'.r_language_server.setup({
+require("lspconfig").r_language_server.setup({
   -- capabilities = custom_attach,
   on_attach = custom_attach,
   flags = { debounc_text_changes = 150 },
@@ -103,21 +102,21 @@ require 'lspconfig'.r_language_server.setup({
 -- }
 
 -- Typescript Setup
-require 'lspconfig'.ts_ls.setup {
+require("lspconfig").ts_ls.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
-  root_dir = require 'lspconfig'.util.root_pattern("package.json")
-}
+  root_dir = require("lspconfig").util.root_pattern("package.json"),
+})
 
 -- Deno Setup
 -- Currently this interferes to much with ts_ls
-require 'lspconfig'.denols.setup({
+require("lspconfig").denols.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
-  root_dir = require 'lspconfig'.util.root_pattern("deno.json", "deno.jsonc")
+  root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
 })
 
-require 'lspconfig'.marksman.setup({
+require("lspconfig").marksman.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
   filetypes = { "md" },
@@ -127,49 +126,51 @@ require 'lspconfig'.marksman.setup({
 --Installation
 --npm install --location=global vscode-langservers-extracted
 --npm install --save vscode-css-languageservice
-require 'lspconfig'.cssls.setup {
-  capabilities = capabilities,
-  on_attach = custom_attach
-}
-
--- TailwindCSS
-require 'lspconfig'.tailwindcss.setup {
-  capabilities = capabilities,
-  on_attach = custom_attach
-}
-
---GO
-require 'lspconfig'.gopls.setup {
+require("lspconfig").cssls.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
-}
+})
 
-require 'lspconfig'.vls.setup {
+-- TailwindCSS
+require("lspconfig").tailwindcss.setup({
+  capabilities = capabilities,
+  on_attach = custom_attach,
+})
+
+--GO
+require("lspconfig").gopls.setup({
+  capabilities = capabilities,
+  on_attach = custom_attach,
+})
+
+require("lspconfig").vls.setup({
   capabilities = capabilities,
   on_attach = custom_attach,
   filetypes = { "vue" },
   cmd = { "vls" },
-}
+})
 
 -- Omnisharp Setup
-require("lsp-format").setup {}
+require("lsp-format").setup({})
 local on_attach = function(client)
-  require "lsp-format".on_attach(client)
-  vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  require("lsp-format").on_attach(client)
+  vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
   custom_attach(client)
 end
 
-require 'lspconfig'.omnisharp.setup {
+require("lspconfig").omnisharp.setup({
   capabilities = capabilities,
   on_attach = on_attach,
   filetypes = { "cs", "csx" },
   handlers = {
-    ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
-    ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
-    ["textDocument/references"] = require('omnisharp_extended').references_handler,
-    ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
+    ["textDocument/definition"] = require("omnisharp_extended").definition_handler,
+    ["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
+    ["textDocument/references"] = require("omnisharp_extended").references_handler,
+    ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
   },
-  cmd = { "dotnet", "/home/austin/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" }
-}
+  cmd = {
+    "/home/austin/Downloads/omnisharp-linux-x64-net6.0/OmniSharp",
+  },
+})
 
-require 'lspconfig'.emmet_language_server.setup(capabilities)
+require("lspconfig").emmet_language_server.setup(capabilities)
